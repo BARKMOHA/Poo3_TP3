6729d2b03a489f27432bed02ac915d35fb3efe6b
babti kane <babti.ak@gmail.com>
2025-08-19 09:51:55 -0400

    modif biblioContext

--- Lipajoli/Lipajoli/Data/BiblioContext.cs
+++ Lipajoli/Lipajoli/Data/BiblioContext.cs
@@ -1,6 +1,16 @@
-﻿namespace Lipajoli.Data
+﻿using Lipajoli.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lipajoli.Data
 {
-    public class BiblioContext
+    public class BiblioContext : DbContext
     {
+        public BiblioContext(DbContextOptions<BiblioContext> options) : base(options) { }
+
+        public DbSet<Livre> Livres { get; set; }
+        public DbSet<Auteur> Auteurs { get; set; }
+        public DbSet<CategorieLivre> Categories { get; set; }
+
+
     }
 }
