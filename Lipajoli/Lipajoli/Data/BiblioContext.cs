2045a7285ba122d0eb0106daed88b3a28a560749
unknown <ben.barka.hb@gmail.com>
2025-08-19 21:50:28 -0400

    ajout classe

--- Lipajoli/Lipajoli/Data/BiblioContext.cs
+++ Lipajoli/Lipajoli/Data/BiblioContext.cs
@@ -1,6 +1,10 @@
-﻿namespace Lipajoli.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lipajoli.Data
 {
-    public class BiblioContext
+    public class BiblioContext : DbContext
     {
+
+        
     }
 }
--- Lipajoli/Lipajoli/obj/Debug/net8.0/Lipajoli.AssemblyInfo.cs
+++ Lipajoli/Lipajoli/obj/Debug/net8.0/Lipajoli.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lipajoli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77b68e0f4359a6558fdf2e1a203a13fdf823555a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8788e9237ae418fce3319ba5385f7e495165e04e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lipajoli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lipajoli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
